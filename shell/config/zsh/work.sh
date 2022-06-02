73fcc79f406c6cb78c208dc14a39b870abd55346
aqshing <aqdebug@gmail.com>
2022-06-02 18:56:52 +0800

    工作中常用的配置

--- shell/config/zsh/work.sh
+++ shell/config/zsh/work.sh
@@ -4,10 +4,23 @@
 # Author: aqshing
 # Email: aqdebug.com aqdebug@gmail.com
 # Brief: 工作中经常用到的别名
-# Created: Tue 31 May 2022 02:52:01 PM CST
-# Changed: Tue 31 May 2022 02:52:01 PM CST
+# Created: 2022-05-31 14:52:01
+# Changed: 2022-06-02 18:49:41
 ###################################################
 
+alias vi='vim'
 alias du='du -sh'
-alias el='vi /greatdb/log'
+alias ansible='python /opt/ansible'
+alias ansible-playbook='python /opt/ansible-playbook'
+
+alias sql='/greatdb/svr/greatdb/bin/greatsql'
+alias sc='/greatdb/sh/sqlnode3316.sh'
+# install greatdb
+alias install='ansible-playbook deploy.yml -i inventory/deploy.ini -v'
+# upgrade greatdb
+alias update='ansible-playbook upgrade.yml -i inventory/upgrade.ini -v'
+alias upgrade='ansible-playbook upgrade.yml -i inventory/upgrade.ini -v'
+# clean greatdb
+alias clean='ansible-playbook cleanup.yml -i inventory/deploy.ini -v'
+##alias el='vi /greatdb/log'
 

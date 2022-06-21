598801b5d5315c2dc2cf0fafbb0b97499889089f
aqshing <aqdebug@gmail.com>
2022-06-21 14:55:50 +0800

    增加了indb updb cldb

--- shell/config/zsh/work.sh
+++ shell/config/zsh/work.sh
@@ -5,7 +5,7 @@
 # Email: aqdebug.com aqdebug@gmail.com
 # Brief: 工作中经常用到的别名
 # Created: 2022-05-31 14:52:01
-# Changed: 2022-06-02 18:49:41
+# Changed: 2022-06-07 18:01:16
 ###################################################
 
 alias vi='vim'
@@ -17,10 +17,22 @@ alias sql='/greatdb/svr/greatdb/bin/greatsql'
 alias sc='/greatdb/sh/sqlnode3316.sh'
 # install greatdb
 alias install='ansible-playbook deploy.yml -i inventory/deploy.ini -v'
+function indb() {
+	cd /opt/greatdb-ansible-master || return 1;
+	ansible-playbook deploy.yml -i inventory/deploy.ini -v
+}
 # upgrade greatdb
 alias update='ansible-playbook upgrade.yml -i inventory/upgrade.ini -v'
 alias upgrade='ansible-playbook upgrade.yml -i inventory/upgrade.ini -v'
+function updb() {
+	cd /opt/greatdb-ansible-master || return 1;
+	ansible-playbook upgrade.yml -i inventory/deploy.ini -v
+}
 # clean greatdb
 alias clean='ansible-playbook cleanup.yml -i inventory/deploy.ini -v'
+function cldb() {
+	cd /opt/greatdb-ansible-master || return 1;
+	ansible-playbook cleanup.yml -i inventory/deploy.ini -v
+}
 ##alias el='vi /greatdb/log'
 
